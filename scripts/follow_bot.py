f71560484c7460506ddc372d834f6d3a2eb4809c
TianqiCS <twang@ualberta.ca>
2019-09-25 13:20:05 -0600

    change

--- scripts/follow_bot.py
+++ scripts/follow_bot.py
@@ -6,6 +6,7 @@ import math
 import ctypes
 import struct
 import time
+from math import copysign
 #import numpy as np
 from kobuki_msgs.msg import BumperEvent, Led
 from nav_msgs.msg import Odometry
@@ -17,256 +18,159 @@ from sensor_msgs.msg import PointCloud2
 
 ## Util Function
 
-def approxEqual(a, b, tol = 0.001):
+def approxEqual(a, b, tol = 0.02):
     return abs(a - b) <= tol
-
-def filterColor(point):
-    rgb_packed = point[3]
-
-    # cast float32 to int so that bitwise operations are possible
-    s = struct.pack('>f' ,rgb_packed)
-    i = struct.unpack('>l',s)[0]
-    # you can get back the float value by the inverse operations
-    pack = ctypes.c_uint32(i).value
-    pt_r = (pack & 0x00FF0000)>> 16
-    pt_g = (pack & 0x0000FF00)>> 8
-    pt_b = (pack & 0x000000FF)
-
-    return approxEqual(pt_r, 0, 20) and approxEqual(pt_g, 0, 20) and approxEqual(pt_b, 0, 20)
-    
-
-##
-
-def readPC(msg):
-    
-    # 0.5 - 1s per loop (too slow!)
-    if msg:
-
-        points = filter(filterColor, point_cloud2.read_points(msg, skip_nans=True))
-
-        for point in points:
-
-            pt_x = point[0]
-            pt_y = point[1]
-            pt_z = point[2]
-            rgb_packed = point[3] 
-            # cast float32 to int so that bitwise operations are possible
-            s = struct.pack('>f' ,rgb_packed)
-            i = struct.unpack('>l',s)[0]
-            # you can get back the float value by the inverse operations
-            pack = ctypes.c_uint32(i).value
-            pt_r = (pack & 0x00FF0000)>> 16
-            pt_g = (pack & 0x0000FF00)>> 8
-            pt_b = (pack & 0x000000FF)
-            rospy.loginfo('Point: x: {0}, y:{1}, z{2}, r:{3}, g:{4}, b:{5}'.format(pt_x, pt_y, pt_z, pt_r, pt_g, pt_b))
-
-
-##State Classes
-class Start(smach.State):
-    def __init__(self):
-        global g_collision, g_odom, g_cmd_vel_pub, g_PC2
-        smach.State.__init__(self, 
-                             outcomes=['success'])
-    def execute(self, userdata):
-        while True:
-            rospy.sleep(1.)
-            readPC(g_PC2)
-        return 'success'
-
-
-# class Parallel(smach.State):
-#     def __init__(self):
-#         global g_collision, g_odom, g_cmd_vel_pub
-#         smach.State.__init__(self, 
-#                              outcomes=['collision', 'end'])
-
-#     def execute(self, userdata):
-#         twist = Twist()
-#         twist.linear.x = 0.1
-#         while True:
-#             rospy.loginfo('Executing state PARALLEL' + '\ng_odom:' + str(g_odom) + '\ng_collision:' + str(g_collision))
-#             g_cmd_vel_pub.publish(twist)
-#             if g_collision:
-#                 return 'collision'
-#             if g_odom['x'] > 3.4:
-#                 twist.linear.x = 0.0
-#                 g_cmd_vel_pub.publish(twist)
-#                 rospy.loginfo('\nBUMPER_BOT ARRIVED!\n')
-#                 return 'end'
-
-# class Backup(smach.State):
-#     def __init__(self):
-#         global g_collision, g_odom, g_cmd_vel_pub
-#         smach.State.__init__(self, 
-#                              outcomes=['success'])
-
-#     def execute(self, userdata):
-#         ori_x = g_odom['x']
-#         ori_y = g_odom['y']
-#         twist = Twist()
-#         twist.linear.x = -0.1
-#         while True:
-#             rospy.loginfo('Executing state BACKUP' + '\ng_odom:' + str(g_odom) + '\ng_collision:' + str(g_collision))
-#             g_cmd_vel_pub.publish(twist)
-#             if abs(ori_x - g_odom['x']) > 0.1 or abs(ori_y - g_odom['y']) > 0.1:
-#                 return 'success'
-#             #elif g_collision:
-#                 #return 'collision'
-
-# class TurnPerpendicular(smach.State):
-#     def __init__(self):
-#         global g_odom,g_cmd_vel_pub
-#         smach.State.__init__(self, 
-#                              outcomes=['success'])
-
-#     def execute(self, userdata):
-#         init_yaw = g_odom['yaw_z']
-#         target_yaw = None
-#         if approxEqual(init_yaw, math.pi/2, 0.2):
-#             target_yaw = -math.pi/2
-#         elif approxEqual(init_yaw, -math.pi/2, 0.2):
-#             target_yaw = math.pi/2
-#         else:
-#             target_yaw = math.pi/2
-            
-#         twist = Twist()
-#         while True:
-#             rospy.loginfo('Executing state TURNPERPENDICULAR' + '\ng_odom:' + str(g_odom))
-#             if approxEqual(g_odom['yaw_z'], target_yaw):
-#                 twist.angular.z = 0
-#                 g_cmd_vel_pub.publish(twist)
-#                 rospy.sleep(1)
-#                 return 'success'
-
-#             if g_odom['yaw_z'] > target_yaw:
-#                 twist.angular.z = -0.3
-#             else:
-#                 twist.angular.z = 0.3
-#             g_cmd_vel_pub.publish(twist)
-
-# class Perpendicular(smach.State):
-#     def __init__(self):
-#         global g_odom, g_collision, g_cmd_vel_pub
-#         smach.State.__init__(self, 
-#                              outcomes=['collision', 'success'])
-
-#     def execute(self, userdata):
-#         old_y = g_odom['y']
-#         twist = Twist()
-#         twist.linear.x = 0.1
-#         while True:
-#             rospy.loginfo('Executing state PERPENDICULAR' + '\ng_odom:' + str(g_odom) + '\ng_collision:' + str(g_collision))
-#             g_cmd_vel_pub.publish(twist)
-#             if abs(g_odom['y'] - old_y) >= 0.6:
-#                 return 'success'
-#             if g_collision:
-#                 return 'collision'
-
-
-# class TurnParallel(smach.State):
-#     def __init__(self):
-#         global g_odom, g_cmd_vel_pub
-#         smach.State.__init__(self, 
-#                              outcomes=['success'])
-
-#     def execute(self, userdata):
-#         twist = Twist()
-#         target_yaw = 0.0
-#         while True:
-#             rospy.loginfo('Executing state TURNPARALLEL' + '\ng_odom:' + str(g_odom))
-#             if approxEqual(g_odom['yaw_z'], target_yaw):
-#                 twist.angular.z = 0
-#                 g_cmd_vel_pub.publish(twist)
-#                 rospy.sleep(1)
-#                 return 'success'
-
-#             if (g_odom['yaw_z'] < target_yaw):
-#                 twist.angular.z = 0.3
-#             elif (g_odom['yaw_z'] > target_yaw):
-#                 twist.angular.z = -0.3
-#             g_cmd_vel_pub.publish(twist)
-  
 ##
 
 ##call_back functions
 
-def odom_callback(msg):
-    global g_odom
-    x = msg.pose.pose.position.x
-    y = msg.pose.pose.position.y
-    yaw = euler_from_quaternion([
-        msg.pose.pose.orientation.x,
-        msg.pose.pose.orientation.y,
-        msg.pose.pose.orientation.z,
-        msg.pose.pose.orientation.w,
-    ])
-    
-    g_odom['x'] = x
-    g_odom['y'] = y
-    g_odom['yaw_z'] = yaw[2]
-
-def bumper_callback(msg):
-    global g_collision
-    g_collision = msg.state
-
 def PC2_callback(msg):
-    global g_PC2
-    g_PC2 = msg
+    global g_centroid, g_std_dist
+    
+    # Initialize the centroid coordinates point count
+    x = y = z = n = 0
+    g_centroid = {}
+
+    # Read in the x, y, z coordinates of all points in the cloud
+    for point in point_cloud2.read_points(msg, skip_nans=True):
+        pt_x = point[0]
+        pt_y = point[1]
+        pt_z = point[2]
+
+        x += pt_x
+        y += pt_y
+        z += pt_z
+        n += 1
+
+    if n > 0:
+        x /= n
+        y /= n
+        z /= n
+
+        g_centroid['x'] = x
+        g_centroid['y'] = y
+        g_centroid['z'] = z
+
+##
+
+## State Class
+
+class Follow(smach.State):
+    
+    def __init__(self):
+
+        smach.State.__init__(self, 
+                             outcomes=['end'])
+
+        self.move_cmd = Twist()
+        
+        # How far away from the goal distance (in meters) before the robot reacts
+        self.z_threshold = rospy.get_param("~z_threshold", 0.05)
+        
+        # How far away from being centered (x displacement) on the person
+        # before the robot reacts
+        self.x_threshold = rospy.get_param("~x_threshold", 0.05)
+        
+        # How much do we weight the goal distance (z) when making a movement
+        self.z_scale = rospy.get_param("~z_scale", 1.0)
+
+        # How much do we weight x-displacement of the person when making a movement        
+        self.x_scale = rospy.get_param("~x_scale", 2.5)
+        
+        # The maximum rotation speed in radians per second
+        self.max_angular_speed = rospy.get_param("~max_angular_speed", 2.0)
+        
+        # The minimum rotation speed in radians per second
+        self.min_angular_speed = rospy.get_param("~min_angular_speed", 0.0)
+        
+        # The max linear speed in meters per second
+        self.max_linear_speed = rospy.get_param("~max_linear_speed", 0.3)
+        
+        # The minimum linear speed in meters per second
+        self.min_linear_speed = rospy.get_param("~min_linear_speed", 0.1)
+        
+        # Slow down factor when stopping
+        self.slow_down_factor = rospy.get_param("~slow_down_factor", 0.8)
+
+    def execute(self, userdata):
+
+        global g_centroid, g_cmd_vel_pub, g_std_dist
+
+        decelerate_factor = 0.9
+        accelerate_factor = 0.5
+        angular_factor = 0.8
+        
+        stop_dist = 0.7
+
+        while True:
+            if g_centroid:
+                # Check our movement thresholds
+                if (abs(g_centroid['z'] - g_std_dist) > self.z_threshold):
+                    # Compute the angular component of the movement
+                    linear_speed = (g_centroid['z'] - g_std_dist) * self.z_scale
+                    
+                    # Make sure we meet our min/max specifications
+                    self.move_cmd.linear.x = copysign(max(self.min_linear_speed, 
+                                            min(self.max_linear_speed, abs(linear_speed))), linear_speed)
+                else:
+                    self.move_cmd.linear.x *= self.slow_down_factor
+                    
+                if (abs(g_centroid['x']) > self.x_threshold):     
+                    # Compute the linear component of the movement
+                    angular_speed = -g_centroid['x'] * self.x_scale
+                    
+                    # Make sure we meet our min/max specifications
+                    self.move_cmd.angular.z = copysign(max(self.min_angular_speed, 
+                                            min(self.max_angular_speed, abs(angular_speed))), angular_speed)
+                else:
+                    # Stop the rotation smoothly
+                    self.move_cmd.angular.z *= self.slow_down_factor
+
+            else:
+                # Stop the robot smoothly
+                self.move_cmd.linear.x *= self.slow_down_factor
+                self.move_cmd.angular.z *= self.slow_down_factor
+
+            print g_centroid
+            #g_cmd_vel_pub.publish(self.move_cmd)
+
+        return 'end'
+
 
 ##
 
 def main():
-    global g_collision, g_odom, g_led_pub, g_cmd_vel_pub
-    g_collision = False
-    g_odom = {'x':0.0, 'y':0.0, 'yaw_z':0.0}
+    global g_cmd_vel_pub, g_std_dist, g_centroid
 
-    rospy.init_node('follow_bot',anonymous=True)
-    rospy.Subscriber('odom', Odometry, odom_callback)
-    rospy.Subscriber('mobile_base/events/bumper', BumperEvent, bumper_callback)
-    rospy.Subscriber("/camera/depth_registered/points", PointCloud2, PC2_callback)
+    g_std_dist = 0.8
+    g_centroid = {}
+
+    rospy.init_node('follow_bot')
+    rospy.Subscriber("point_cloud", PointCloud2, PC2_callback, queue_size=1)
+    rospy.loginfo("Subscribing to point cloud...")
+    
+    # Wait for the pointcloud topic to become available
+    rospy.wait_for_message('point_cloud', PointCloud2)
+
+    rospy.loginfo("Ready to follow!")
 
     g_cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    g_led_pub = rospy.Publisher('/mobile_base/commands/led1', Led, queue_size=10)
 
     sm = smach.StateMachine(outcomes=['end'])
 
     with sm:
-        smach.StateMachine.add('Start', Start(), transitions={'success':'end'}) 
-        # smach.StateMachine.add('Parallel', Parallel(),
-        #                         transitions={'end':'end',
-        #                                      'collision': 'Backup'})   
-
-        # smach.StateMachine.add('Backup', Backup(),
-        #                         transitions={'success':'TurnPerpendicular'})
-
-        # smach.StateMachine.add('TurnPerpendicular', TurnPerpendicular(),
-        #                         transitions={'success':'Perpendicular'})
-
-        # smach.StateMachine.add('Perpendicular', Perpendicular(),
-        #                         transitions={'success':'TurnParallel',
-        #                                      'collision': 'Backup'})
-
-        # smach.StateMachine.add('TurnParallel', TurnParallel(),
-        #                         transitions={'success':'Parallel'}) 
-
-    # Create and start the introspection server
-    sis = smach_ros.IntrospectionServer('root', sm, '/SM_ROOT')
-    sis.start()
+        smach.StateMachine.add('Follow', Follow(),
+                                transitions={'end':'end'})  
 
     outcome = sm.execute()
-    if outcome == 'end':
-        led = Led()
-        led.value = 1
-        g_led_pub.publish(led)
 
     # Wait for ctrl-c to stop the application
     rospy.spin()
-    sis.stop()
+    
+    return
 
 if __name__ == '__main__':
-    g_collision = None
-    g_odom = None
-    g_led_pub = None
     g_cmd_vel_pub = None
-    g_PC2 = None
+    g_std_dist = None
+    g_centroid = None
     main()
